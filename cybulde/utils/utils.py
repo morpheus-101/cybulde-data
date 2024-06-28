bf46b3efc38a44ab4d36ad06ff212de3eabaafd3
Rishikesh Dhayarkar <rishikeshdhayarkar1091@gmail.com>
2024-06-28 05:54:16 +0200

    Updated version of the data from v0 to v1

--- cybulde/utils/utils.py
+++ cybulde/utils/utils.py
@@ -2,13 +2,12 @@ import logging
 import socket
 import subprocess
 
+logging.basicConfig()
+
 
 def get_logger(name: str) -> logging.Logger:
     return logging.Logger(f"[{socket.gethostname()}] {name}")
 
 
 def run_shell_command(cmd: str) -> str:
-    return subprocess.run(cmd, text=True, 
-                          shell=True, 
-                          check=True, 
-                          capture_output=True).stdout
+    return subprocess.run(cmd, text=True, shell=True, check=True, capture_output=True).stdout
